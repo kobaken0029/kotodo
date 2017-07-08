e8974cc4ee2ee16a4595bf3b3fe1f50f99094a70
kobayashi_kento <kobaken0029@gmail.com>
2017-07-08 15:27:20 +0900

    Fixed databinding

--- app/src/main/java/com/kobaken0029/kotodo/MainActivity.kt
+++ app/src/main/java/com/kobaken0029/kotodo/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.kobaken0029.kotodo
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.kobaken0029.kotodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }

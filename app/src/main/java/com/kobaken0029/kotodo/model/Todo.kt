411bc1fc8df9f08f8741efdb3e2a64cf32559e74
kobayashi_kento <kobaken0029@gmail.com>
2017-07-08 16:51:22 +0900

    Add todo method

--- app/src/main/java/com/kobaken0029/kotodo/model/Todo.kt
+++ app/src/main/java/com/kobaken0029/kotodo/model/Todo.kt
@@ -1,3 +1,11 @@
 package com.kobaken0029.kotodo.model
 
-data class Todo(val content: String)
+data class Todo(val content: String) {
+    fun create() {
+        TODO("作成します")
+    }
+
+    fun delete() {
+        TODO("削除します")
+    }
+}

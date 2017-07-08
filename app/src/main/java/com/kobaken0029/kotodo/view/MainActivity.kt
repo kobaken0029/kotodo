e1606b5659706e1058ad7ccec3257289a65d9a18
kobayashi_kento <kobaken0029@gmail.com>
2017-07-08 16:50:42 +0900

    Create view package

--- app/src/main/java/com/kobaken0029/kotodo/view/MainActivity.kt
+++ app/src/main/java/com/kobaken0029/kotodo/view/MainActivity.kt
@@ -1,9 +1,12 @@
-package com.kobaken0029.kotodo
+package com.kobaken0029.kotodo.view
 
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.kobaken0029.kotodo.R
 import com.kobaken0029.kotodo.databinding.ActivityMainBinding
+import com.kobaken0029.kotodo.model.Todo
+import com.kobaken0029.kotodo.view.adapter.TodoAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val adapter = TodoAdapter(applicationContext, listOf(Todo("hoge"), Todo("piyo")))
+        binding.todoList.adapter = adapter
+
+        binding.todo = Todo("")
     }
 }

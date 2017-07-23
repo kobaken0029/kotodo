ff39b36c6ee11a3f9f872d6c8d4bb642cd417473
kobayashi_kento <kobaken0029@gmail.com>
2017-07-24 06:03:52 +0900

    Fixed now on DateUtil

--- app/src/main/java/com/kobaken0029/kotodo/util/DateUtil.kt
+++ app/src/main/java/com/kobaken0029/kotodo/util/DateUtil.kt
@@ -1,9 +1,10 @@
 package com.kobaken0029.kotodo.util
 
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.format.DateTimeFormatter
 
 object DateUtil {
     fun now(): String {
-        return LocalDateTime.now().toString()
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss"))
     }
 }
